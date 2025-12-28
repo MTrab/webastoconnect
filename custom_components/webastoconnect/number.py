26fd9b30c3a04da224e9cdb948c8d0cf133bdfc8
Malene Trab <malene@trab.dk>
2025-12-28 15:52:27 +0000

    Migrate number

--- custom_components/webastoconnect/number.py
+++ custom_components/webastoconnect/number.py
@@ -14,7 +14,7 @@ from homeassistant.helpers.update_coordinator import (
 from homeassistant.util import slugify as util_slugify
 
 from .api import WebastoConnectUpdateCoordinator
-from .base import WebastoConnectNumberEntityDescription
+from .base import WebastoBaseEntity, WebastoConnectNumberEntityDescription
 from .const import ATTR_COORDINATOR, DOMAIN
 
 LOGGER = logging.getLogger(__name__)
@@ -67,54 +67,26 @@ async def async_setup_entry(hass, entry: ConfigEntry, async_add_devices):
     async_add_devices(numbers_list)
 
 
-class WebastoConnectNumber(
-    CoordinatorEntity[DataUpdateCoordinator[None]], NumberEntity
-):
+class WebastoConnectNumber(WebastoBaseEntity, NumberEntity):
     """Representation of a Webasto Connect number."""
 
     def __init__(
         self,
-        device_id: str,
+        device_id: int,
         description: WebastoConnectNumberEntityDescription,
         coordinator: WebastoConnectUpdateCoordinator,
     ) -> None:
         """Initialize a Webasto Connect number."""
-        super().__init__(coordinator)
-
-        self.entity_description = description
-        self._config = coordinator.entry
-        self.coordinator = coordinator
-        self._hass = coordinator.hass
-        self._device_id = device_id
-
-        self._attr_name = self.entity_description.name
-        self._attr_unique_id = util_slugify(
-            f"{self.coordinator.cloud.devices[self._device_id].device_id}_{self._attr_name}"
-        )
-        self._attr_should_poll = False
-
-        self._attr_device_info = {
-            "identifiers": {(DOMAIN, self._device_id)},
-            "name": self.coordinator.cloud.devices[self._device_id].name,
-            "model": "ThermoConnect",
-            "manufacturer": "Webasto",
-            "hw_version": self.coordinator.cloud.devices[self._device_id].settings[
-                "hw_version"
-            ],
-            "sw_version": self.coordinator.cloud.devices[self._device_id].settings[
-                "sw_version"
-            ],
-            "configuration_url": "https://my.webastoconnect.com",
-        }
+        super().__init__(device_id, coordinator, description)
 
         if not isinstance(description.unit_fn, type(None)):
             self._attr_native_unit_of_measurement = description.unit_fn(
-                coordinator.cloud.devices[self._device_id]
+                self._cloud.devices[self._device_id]
             )
 
         self.entity_id = number.ENTITY_ID_FORMAT.format(
             util_slugify(
-                f"{self.coordinator.cloud.devices[self._device_id].name} {self._attr_name}"
+                f"{self._cloud.devices[self._device_id].name} {self._attr_name}"
             )
         )
 
@@ -123,17 +95,15 @@ class WebastoConnectNumber(
         """Get the native value."""
         return cast(
             float,
-            self.entity_description.value_fn(
-                self.coordinator.cloud.devices[self._device_id]
-            ),
+            self.entity_description.value_fn(self._cloud.devices[self._device_id]), # type: ignore
         )
 
     async def async_set_native_value(self, value: float) -> None:
         """Set new value."""
         LOGGER.debug("Setting '%s' to '%s'", self.entity_id, value)
         await self._hass.async_add_executor_job(
-            self.entity_description.set_fn,
-            self.coordinator.cloud.devices[self._device_id],
+            self.entity_description.set_fn, # type: ignore
+            self._cloud.devices[self._device_id],
             value,
         )
         await self.coordinator.async_refresh()
